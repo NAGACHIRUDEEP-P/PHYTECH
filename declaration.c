b1c5c96ad8b59b66c50e1afc8c27fdc0ff449af1
PURUGUNDLA NAGA CHIRUDEEP <166825596+NAGACHIRUDEEP-P@users.noreply.github.com>
2025-03-05 04:22:07 +0530

    declaration.c

---
+++ declaration.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int myInt = 10;       // Integer variable
+    float myFloat = 3.14; // Floating-point variable
+    char myChar = 'A';    // Character variable
+
+    // Printing the values
+    printf("Integer: %d\n", myInt);
+    printf("Float: %.2f\n", myFloat);
+    printf("Character: %c\n", myChar);
+
+    return 0;
+}
